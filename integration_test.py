634af97f6bf116c3d8e39a9ed1f140e3e28e2b0d
Artem <90005692+thebordelie@users.noreply.github.com>
2024-02-14 20:54:38 +0300

    Update integration_test.py

--- integration_test.py
+++ integration_test.py
@@ -28,4 +28,5 @@ def test_translator_and_machine(golden, caplog):
         with open(target, encoding="utf-8") as file:
             code = file.read()
         assert code == golden.out["out_code"]
+        assert stdout.getvalue() == golden.out["out_stdout"]
         assert caplog.text.strip() == golden.out["out_log"]
